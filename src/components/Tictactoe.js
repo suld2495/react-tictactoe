8d5c16c7c3d7648e9bcff51f3abebcb340d49f2c
suld2495 <suld2495@naver.com>
2020-06-30 12:05:05 +0900

    기능추가: Tictactoe를 구성하는 컴포넌트의 기본 코드 생성

---
+++ src/components/Tictactoe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './Game';
+
+export default class Tictactoe extends React.Component {
+    render() {
+        return (
+            <div>
+                <Game />
+            </div>
+        )
+    }
+}
